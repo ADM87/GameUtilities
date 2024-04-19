0bb3ac6ae2feb49512bd70ade2efa022fe153d0a
ADM87 <aaron.d.malley@gmail.com>
2024-04-19 11:07:42 -0300

    Added additional pass service mocks and assosiated tests

--- GameUtilities.Tests/Tests.Pass/Tests/ServiceProvider/PassMockServices.cs
+++ GameUtilities.Tests/Tests.Pass/Tests/ServiceProvider/PassMockServices.cs
@@ -2,24 +2,38 @@ using ADM87.GameUtilities.ServiceProvider;
 
 namespace GameUtilities.Tests
 {
-    public interface ISimpleCollectedService {}
-    [ServiceDefinition(typeof(ISimpleCollectedService))]
-    public class SimpleCollectedService : ISimpleCollectedService {}
+    public interface ICollectedService {}
+    [ServiceDefinition(typeof(ICollectedService))]
+    public class CollectedService : ICollectedService {}
 
     public interface ICollectedSingletonService {}
     [ServiceDefinition(typeof(ICollectedSingletonService), isSingleton: true)]
     public class CollectedSingletonService : ICollectedSingletonService {}
 
-    public interface ISimpleManualService {}
-    public class SimpleManualService : ISimpleManualService {}
+    public interface ICollectedServiceWidthDependency
+    {
+        ICollectedService ASimpleCollectedService { get; }
+    }
+    [ServiceDefinition(typeof(ICollectedServiceWidthDependency))]
+    public class CollectedServiceWithDependency : ICollectedServiceWidthDependency
+    {
+        [ServiceDependency]
+        public ICollectedService ASimpleCollectedService { get; private set; }
+    }
+
+    public interface IManualService {}
+    public class ManualService : IManualService {}
+
+    public interface IManualSingletonService {}
+    public class ManualSingletonService : IManualSingletonService {}
 
     public interface IManualServiceWidthDependency
     {
-        ISimpleManualService ASimpleManualService { get; }
+        IManualService ASimpleManualService { get; }
     }
     public class ManualServiceWithDependency : IManualServiceWidthDependency
     {
         [ServiceDependency]
-        public ISimpleManualService ASimpleManualService { get; private set; }
+        public IManualService ASimpleManualService { get; private set; }
     }
 }
