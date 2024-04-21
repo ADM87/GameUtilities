4fec6f54624fdb324bdd9b573308a93dbcbaebc2
ADM87 <40219687+ADM87@users.noreply.github.com>
2024-04-21 01:42:13 -0300

    Stubbed out test projects. Added workflow to build and run tests.
    * Stubbed out test projects. Adding first pass at test jobs
    
    * Adjusted workflow. Second attempt
    
    * Adjusted the version of .NET. Third attempt

---
+++ GameUtilities.Tests/Tests.Fail/Tests/SetupFailTests.cs
@@ -0,0 +1,18 @@
+namespace GameUtilities.Tests
+{
+    [SetUpFixture]
+    public class SetupFailTests
+    {
+        [OneTimeSetUp]
+        public void BeforeTests()
+        {
+            // Add your setup code here
+        }
+
+        [OneTimeTearDown]
+        public void AfterTests()
+        {
+            // Add your teardown code here
+        }
+    }
+}
---
+++ GameUtilities.Tests/Tests.Fail/Tests/Tests.cs
@@ -0,0 +1,24 @@
+namespace GameUtilities.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.Pass();
+        }
+    }
+}
