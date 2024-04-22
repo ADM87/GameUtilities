ccd42a9deae4468f9cd3d4879cb177f62320a257
ADM87 <aaron.d.malley@gmail.com>
2024-04-22 17:06:43 -0300

    meow

--- .github/scripts/create_tag.sh
+++ .github/scripts/create_tag.sh
@@ -9,8 +9,11 @@ if [[ -z "$PREFIX" ]]; then
     exit 1
 fi
 
+# Fetch tags from remote repository
+git fetch --tags
+
 # Get the last tag that matches the versioning pattern major.minor.patch
-last_tag=$(git describe --tags --match="v[0-9]*.[0-9]*.[0-9]*" --abbrev=0)
+last_tag=$(git describe --tags --match="v[0-9]*.[0-9]*.[0-9]*" --abbrev=0 HEAD)
 
 # Default to v0.0.0 if no previous tags exist
 last_tag=${last_tag:-"v0.0.0"}
