4b89609fb6edc9044508acc0d8ff0084256b2342
ADM87 <40219687+ADM87@users.noreply.github.com>
2024-04-22 10:58:28 -0300

    Patch/deploy unity (#14)
    * Some updates
    
    * Setting config token
    
    * Setting config token
    
    * fixing stamp workflow
    
    * Setting config token
    
    * fixing stamp workflow

--- .stamp/scripts/stamp.sh
+++ .stamp/scripts/stamp.sh
@@ -27,25 +27,18 @@ last_tag=$(git tag -l --sort=-v:refname 'v[0-9]*.[0-9]*.[0-9]*' | head -n 1)
 # Default to v0.0.0 if no previous tags exist
 last_tag=${last_tag:-"v0.0.0"}
 
-echo "Current version: $last_tag"
-
 # Parse major, minor, and patch versions from last_tag
 IFS='.' read -r major_version minor_version patch_version <<< "${last_tag#v}"
 
-echo "Branch: $BRANCH_NAME"
-
 # Check for major/ prefix
 if [[ "$BRANCH_NAME" == major/* ]]; then
-    echo "Incrementing major version."
     major_version=$((major_version + 1))
     minor_version=0
     patch_version=0
 elif [[ "$BRANCH_NAME" == minor/* ]]; then
-    echo "Incrementing minor version."
     minor_version=$((minor_version + 1))
     patch_version=0
 elif [[ "$BRANCH_NAME" == patch/* ]]; then
-    echo "Incrementing patch version."
     patch_version=$((patch_version + 1))
 else
     echo "Current branch does not have major/, minor/, or patch/ prefix" >&2
@@ -55,16 +48,4 @@ fi
 
 # Create a new version tag
 new_tag="v${major_version}.${minor_version}.${patch_version}"
-echo "New version: $new_tag"
-
-if ! git tag -a "$new_tag" -m "Version $new_tag" >&2; then
-    echo "Failed to create tag." >&2
-    exit 1
-fi
-
-if ! git push origin "$new_tag" >&2; then
-    echo "Failed to push tag." >&2
-    exit 1
-fi
-
-echo "Tag created and pushed successfully."
+echo "$new_tag"
